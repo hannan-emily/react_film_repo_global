b57ad0409ed6e08b956b9a470eacdd7e492fdca9
hannan-emily <emilyhannan@gmail.com>
2018-03-09 16:55:28 -0800

    stopping point while refactoring components

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FilmPoster from './FilmPoster';
 import Fave from './Fave';
 
-class FilmRow extends Component {
+const FilmRow = (props) => {
 
 
 
-  render() {
-
-    const { title, date, key, url } = this.props
+    const { title, date, key, url } = props
 
     const posterUrl = "https://image.tmdb.org/t/p/w780/" + url
     const year = new Date(date).getFullYear()
 
 
     return (
-      <div className="film-row" onClick={this.props.onDetailsClick}>
+      <div className="film-row" onClick={props.onDetailsClick}>
         <FilmPoster title={title} url={url} />
         <div className="film-summary">
           <h1>{title}</h1>
           <p>{year}</p>
         </div>
-        <Fave isFave={this.props.isFave} onFaveToggle={this.props.onFaveToggle} />
+        <Fave isFave={props.isFave} onFaveToggle={props.onFaveToggle} />
       </div>
     )
-  }
 }
 
 export default FilmRow;
